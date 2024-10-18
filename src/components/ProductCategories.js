748b6e703f740529d1b3e399c8d1fe56057526b8
nikhil_is_here <na52m2002@gmail.com>
2024-10-18 21:49:40 +0530

    correction

--- src/components/ProductCategories.js
+++ src/components/ProductCategories.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const ProductCategories = ({categoryName,imageS,goTo="/",activeCat,catSlug}) => {
   return (
     <div className="col-md-6 col-lg-3 text-center col-6">
-    <div className={`catergoary-box ${activeCat===catSlug?"cat-active":""} `}>
+    <div className={`catergoary-box ${(activeCat===catSlug && activeCat && catSlug)?"cat-active":""} `}>
      <Link href={goTo}>
       <Image layout='responsive' height={75} width={250} src={imageS} alt='logo'/>
         <div className="pros-txt mt-3">{categoryName}</div>
