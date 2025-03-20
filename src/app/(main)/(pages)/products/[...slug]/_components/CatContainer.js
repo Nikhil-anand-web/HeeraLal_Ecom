226167bb924bdfe62db4b24a48fa3d2b964c3e0d
nikhil_is_here <na52m2002@gmail.com>
2025-03-20 19:13:32 +0530

    err

--- src/app/(main)/(pages)/products/[...slug]/_components/CatContainer.js
+++ src/app/(main)/(pages)/products/[...slug]/_components/CatContainer.js
@@ -21,6 +21,8 @@ const CatContainer = ({ categories, categorySlug }) => {
     const [height, setHeight] = useState(0);
 
     useEffect(() => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
         const handleResize = () => {
             setWidth(window.innerWidth);
             setHeight(window.innerHeight);
@@ -34,15 +36,15 @@ const CatContainer = ({ categories, categorySlug }) => {
         <>
             <Swiper
                 onSwiper={setSwiperRef}
-                slidesPerView={width>=996?5:3}
+                slidesPerView={width >= 996 ? 5 : 3}
                 centeredSlides={true}
                 // spaceBetween={15}
-                navigation={width>=996}
+                navigation={width >= 996}
                 modules={[Pagination, Navigation]}
                 className="mySwiper"
             >
-                
-                {categories.map(cat => <SwiperSlide style={{margin:"10px"}}> <ProductCategories activeCat={categorySlug} imageS={cat.image[0].url} catSlug={cat.slug} categoryName={cat.categoryName} goTo={`/products/${cat.slug}`} /> </SwiperSlide>)}
+
+                {categories.map(cat => <SwiperSlide style={{ margin: "10px" }}> <ProductCategories activeCat={categorySlug} imageS={cat.image[0].url} catSlug={cat.slug} categoryName={cat.categoryName} goTo={`/products/${cat.slug}`} /> </SwiperSlide>)}
 
 
 
@@ -54,7 +56,7 @@ const CatContainer = ({ categories, categorySlug }) => {
 
 
         </>
-        
+
     )
 }
 
